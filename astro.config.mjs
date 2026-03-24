94cb4154bce528933607dd03300123aa26fc37a6
A-PachecoT <andre.pacheco.t@uni.pe>
2026-03-24 17:04:29 +0100

    Fix: Remove base path for custom domain (CSS 404 fix)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-carolina-corzo",
+  site: "https://carolina-corzo.cofoundy.dev",
   vite: {
     plugins: [tailwindcss()],
   },
